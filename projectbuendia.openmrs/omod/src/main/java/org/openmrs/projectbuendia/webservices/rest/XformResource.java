ff81279ce753004dc5ea13f9348f70eda5831479
Jon Skeet <jonskeet@google.com>
2014-11-12 09:44:19 +0000

    First pass at the XformResource controller

--- projectbuendia.openmrs/omod/src/main/java/org/openmrs/projectbuendia/webservices/rest/XformResource.java
+++ projectbuendia.openmrs/omod/src/main/java/org/openmrs/projectbuendia/webservices/rest/XformResource.java
@@ -3,7 +3,6 @@ package org.openmrs.projectbuendia.webservices.rest;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
