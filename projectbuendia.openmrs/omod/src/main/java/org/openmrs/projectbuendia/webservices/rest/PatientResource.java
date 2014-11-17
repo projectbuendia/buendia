37be5b35c85c8500ccdd6b5b24378f1d571de8af
Nick Fortescue <nfortescue@google.com>
2014-11-17 16:08:16 +0000

    Added adding a single patient without Xforms to make testing easier. This code will probably get thrown away.

--- projectbuendia.openmrs/omod/src/main/java/org/openmrs/projectbuendia/webservices/rest/PatientResource.java
+++ projectbuendia.openmrs/omod/src/main/java/org/openmrs/projectbuendia/webservices/rest/PatientResource.java
@@ -1,6 +1,6 @@
 package org.openmrs.projectbuendia.webservices.rest;
 
-import org.openmrs.Patient;
+import org.openmrs.*;
 import org.openmrs.api.PatientService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.webservices.rest.SimpleObject;
@@ -8,14 +8,17 @@ import org.openmrs.module.webservices.rest.web.RequestContext;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
 import org.openmrs.module.webservices.rest.web.representation.Representation;
+import org.openmrs.module.webservices.rest.web.resource.api.Creatable;
 import org.openmrs.module.webservices.rest.web.resource.api.Listable;
 import org.openmrs.module.webservices.rest.web.resource.api.Retrievable;
 import org.openmrs.module.webservices.rest.web.resource.api.Searchable;
+import org.openmrs.module.webservices.rest.web.response.ConversionException;
 import org.openmrs.module.webservices.rest.web.response.ResponseException;
 import org.projectbuendia.openmrs.webservices.rest.RestController;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -23,8 +26,15 @@ import java.util.List;
  * Note: this is under org.openmrs as otherwise the resource annotation isn't picked up.
  */
 @Resource(name = RestController.REST_VERSION_1_AND_NAMESPACE + "/patient", supportedClass = Patient.class, supportedOpenmrsVersions = "1.10.*")
-public class PatientResource implements Listable, Searchable, Retrievable {
+public class PatientResource implements Listable, Searchable, Retrievable, Creatable {
 
+    private static final String GENDER = "gender";
+    private static final String GIVEN_NAME = "given_name";
+    private static final String FAMILY_NAME = "family_name";
+    private static final User CREATOR = new User(1);
+    private static final String ID = "id";
+    private static final String MSF_IDENTIFIER = "MSF";
+    public static final Location LOCATION = new Location(1);
     private final PatientService patientService;
 
     public PatientResource() {
@@ -46,13 +56,66 @@ public class PatientResource implements Listable, Searchable, Retrievable {
 
     private SimpleObject patientToJson(Patient patient) {
         SimpleObject jsonForm = new SimpleObject();
-        jsonForm.add("id", patient.getUuid() /*TODO(nfortescue): patient.getPatientIdentifier().getIdentifier()*/);
-        jsonForm.add("given_name", patient.getGivenName());
-        jsonForm.add("family_name", patient.getFamilyName());
+        jsonForm.add(ID, patient.getUuid() /*TODO(nfortescue): patient.getPatientIdentifier().getIdentifier()*/);
+        jsonForm.add(GIVEN_NAME, patient.getGivenName());
+        jsonForm.add(FAMILY_NAME, patient.getFamilyName());
         jsonForm.add("status", "probable" /* TODO(nfortescue): work out how to store this */);
-        jsonForm.add("gender", patient.getGender());
+        jsonForm.add(GENDER, patient.getGender());
         jsonForm.add("created_timestamp_utc", patient.getDateCreated().getTime());
         return jsonForm;
+    }
+
+    @Override
+    public Object create(SimpleObject simpleObject, RequestContext requestContext) throws ResponseException {
+        // We really want this to use XForms, but lets have a simple default implementation for early testing
+
+        if (!simpleObject.containsKey(ID)) {
+            throw new ConversionException("No id set in create request");
+        }
+        PatientIdentifierType identifierType = patientService.getPatientIdentifierTypeByName(MSF_IDENTIFIER);
+        ArrayList<PatientIdentifierType> identifierTypes = new ArrayList<>();
+        identifierTypes.add(identifierType);
+        String id = (String)simpleObject.get(ID);
+        List<Patient> existing =
+                patientService.getPatients(null, id, identifierTypes, true /* exact identifier match */);
+        if (!existing.isEmpty()) {
+            throw new ConversionException("Creating an object that already exists " + id);
+        }
+
+        Patient patient = new Patient();
+        // TODO(nfortescue): do this properly from authentication
+        patient.setCreator(CREATOR);
+        patient.setDateCreated(new Date());
+
+        if (simpleObject.containsKey(GENDER)) {
+            patient.setGender((String)simpleObject.get(GENDER));
+        }
+
+        PersonName pn = new PersonName();
+        if (simpleObject.containsKey(GIVEN_NAME)) {
+            pn.setGivenName((String)simpleObject.get(GIVEN_NAME));
+        }
+        if (simpleObject.containsKey(FAMILY_NAME)) {
+            pn.setFamilyName((String) simpleObject.get(FAMILY_NAME));
+        }
+
+        pn.setCreator(patient.getCreator());
+        pn.setDateCreated(patient.getDateCreated());
+        patient.addName(pn);
+
+        // Fake location
+        PatientIdentifier identifier = new PatientIdentifier();
+        identifier.setCreator(patient.getCreator());
+        identifier.setDateCreated(patient.getDateCreated());
+        identifier.setIdentifier(id);
+
+        identifier.setIdentifierType(identifierType);
+        identifier.setPreferred(true);
+        patient.addIdentifier(identifier);
+
+        patientService.savePatient(patient);
+
+        return simpleObject;
     }
 
     @Override
