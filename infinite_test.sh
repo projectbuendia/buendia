f770c59da2774c4e776010a69c46bedbaeeffb1d
Pim De Witte <pim@whitespell.com>
2014-11-19 14:14:16 +0000

    another update

--- infinite_test.sh
+++ infinite_test.sh
@@ -43,9 +43,10 @@ dd if=$DATA_SOURCE of=$WORKSPACE/sample bs=$BLOCK_SIZE count=1
          		# if copying fails, check if disk is more than or 99% full. Then check if first file is still in tact. If true, retry.
 			df -h $CHECK_DIR | awk '{ print $5 }' | cut -d'%' -f1 | grep -o '[0-9]*' |  while read usep; do
 				if [[ $usep = *[[:digit:]]* ]]; then
+					#check if the error was thrown because the disk was full
 				 	if [ $usep -ge 99 ]; then
 				 		#enable to do disk ful write
-				 		cmp --silent $WORKSPACE/sample $CHECK_DIR/sample_data/0.sample && echo "TEST SUCCESS $COUNTER files, 0.sample still in tact and disk full \n" >> diskfull.log  || echo "TEST FAIL $COUNTER files, Index 0 corrupted when disk full \n" >> diskfull.log
+				 		cmp --silent $WORKSPACE/sample $CHECK_DIR/sample_data/0.sample && echo "TEST SUCCESS $COUNTER files, 0.sample still in tact and disk full \n" >> diskfull.log && let SUCCESS=SUCCESS+1  || echo "TEST FAIL $COUNTER files, Index 0 corrupted when disk full \n" >> diskfull.log && let FAILS=FAILS+1
 				 		cat diskfull.log
 				 		let DISKFULL=DISKFULL+1 
 				 		if [ $INFINITE -eq 1 ]; then 
@@ -54,23 +55,20 @@ dd if=$DATA_SOURCE of=$WORKSPACE/sample bs=$BLOCK_SIZE count=1
 						else
 							exit
 						fi
+					else 
+						#if error wasn't thrown because disk as full, another error was thrown causing the test to fail
+						let FAILS=FAILS+1 
 					fi
 				fi
-			done
-         		let FAILS=FAILS+1 
+			done        		
         	fi
         	let COUNTER=COUNTER+1 
-        	item=$FAILS
+        	item=$SUCCESS
 			total=$COUNTER
 			df -h $CHECK_DIR | awk '{ print $5 }' | cut -d'%' -f1 | grep -o '[0-9]*' |  while read usep; do
 				if [[ $usep = *[[:digit:]]* ]]; then
 					percent=$(printf '%i %i' $item $total | awk '{ pc=100*$1/$2; i=int(pc); print (pc-i<0.5)?i:i+1 }')
-					echo -ne "  Disk Full: $usep%, Fails: $FAILS($percent%), Graceful fails (disk full): $DISKFULL in $COUNTER rounds"
+					echo -ne "  Success: Disk Full: $usep%, Fails: $FAILS ($percent%), Graceful fails (disk full): $DISKFULL,  $COUNTER rounds \r"
 				fi
 			done
          done
-
-
- 
-
-#check whether write succeeded, if not ( check if error message contains the word full or space) if not, write error to error.log and add a fail +1
