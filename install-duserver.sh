04c771de76f10ac9fcec0ed043c10a3f65a705e1
mslatour <sanderlatour@gmail.com>
2014-12-03 17:15:13 +0000

    Added first version of the duserver install script

---
+++ install-duserver.sh
@@ -0,0 +1,6 @@
+apt-get -y install nginx
+mkdir /usr/share/nginx/www/openmrs
+mkdir /usr/share/nginx/www/androidclient
+
+#TODO: wget a script that needs to be run when a usb drive is entered
+#TODO: add udev rule file that triggers the script to be run on 'add'
