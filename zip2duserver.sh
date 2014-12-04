50885066afc573c1566d834c3665370eba33eb0d
mslatour <sanderlatour@gmail.com>
2014-12-04 01:14:12 +0000

    Ensured www-data would be the user copying the files in the duserver

--- zip2duserver.sh
+++ zip2duserver.sh
@@ -20,36 +20,48 @@ archive=$1
 
 ext="${1##*.}"
 
-out=`mktemp -d tmp.XXXXXXXXXX`
+extractdir=`mktemp -d /tmp/tmp.XXXXXXXXXX`
 
 if [ "$ext" == "zip" ]; then
 	echo -n "Extracting zip file..."
-	unzip $archive -d $out > /dev/null
+	unzip $archive -d $extractdir > /dev/null
 	print_success $?
 fi
 
 if [ "$ext" == "tar" ]; then
 	echo -n "Extracting tar file..."
-	tar -xf $archive -C $out > /dev/null
+	tar -xf $archive -C $extractdir > /dev/null
 	print_success $?
 fi
 
 if [ "$ext" == "tgz" ] || [ "$ext" == "gz" ]; then
 	echo -n "Extracting tar.gz file..."
-	tar -xzf $archive -C $out > /dev/null
+	tar -xzf $archive -C $extractdir > /dev/null
 	print_success $?
 fi
 
 if [ "$ext" == "bz2" ]; then
 	echo -n "Extracting tar.bz2 file..."
-	tar -xjf $archive -C $out > /dev/null
+	tar -xjf $archive -C $extractdir > /dev/null
 	print_success $?
 fi
 
-echo -n "Uploading extracted files to 104.155.15.141..."
-scp -r $out/* duserver.projectbuendia.org:/var/www/versions/ > /dev/null
+uploaddir=`ssh duserver.projectbuendia.org "mktemp -d /tmp/tmp.XXXXXXXXXX"`
+
+echo -n "Uploading extracted files to a temporary folder on the duserver..."
+scp -r $extractdir/* duserver.projectbuendia.org:$uploaddir > /dev/null
 print_success $?
 
-echo -n "Removing extracted files..."
-rm -R $out
+echo -n "Removing extracted files locally..."
+rm -R $extractdir
+print_success $?
+
+echo -n "Deploying the extracted files..."
+ssh -q duserver.projectbuendia.org <<ENDSSH > /dev/null
+sudo chown -R www-data:www-data $uploaddir
+sudo su www-data
+cp -R $uploaddir/* /var/www/versions/
+exit
+sudo rm -R $uploaddir
+ENDSSH
 print_success $?
