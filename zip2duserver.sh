8ffb80938db6975f72bb050f797abc280d13007f
mslatour <sanderlatour@gmail.com>
2014-12-05 12:06:20 +0000

    Different sourcing

--- zip2duserver.sh
+++ zip2duserver.sh
@@ -60,7 +60,7 @@ echo -n "Deploying the extracted files..."
 ssh -q packages.projectbuendia.org <<ENDSSH > /dev/null
 sudo chown -R www-data:www-data $uploaddir
 sudo su www-data
-/bin/bash /etc/profile
+. /etc/profile
 cp -R $uploaddir/* /var/www/packages/
 duserver_make_index.py
 exit
