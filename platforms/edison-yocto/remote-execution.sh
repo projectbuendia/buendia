279f1fed8173f6f16f53ec8250fd1c3a3d6acf87
akalachman <akalachman@gmail.com>
2014-12-10 17:10:02 +0000

    Merging changes to help prevent network disruptions from affecting installation

--- platforms/edison-yocto/remote-execution.sh
+++ platforms/edison-yocto/remote-execution.sh
@@ -1,8 +1,8 @@
 # Utility functions for executing shell commands and scripts on an Edison.
 
+export TARGET_IPADDR
 if [ ! -n "$TARGET_IPADDR" ]; then
-  export TARGET_IPADDR=192.168.2.15
-  echo "Using default target address $TARGET_IPADDR."
+  TARGET_IPADDR=192.168.2.15
 fi
 
 target="root@$TARGET_IPADDR"
@@ -59,4 +59,57 @@ function run_script_in_debian() {
 
   echo ">> $target(debian): $name" "$@" 1>&2
   $ssh $target /usr/local/bin/enter-debian $name "$@"
+}
+
+function connect_linux_ethernet() {
+  if [[ "$OSTYPE" == linux* ]]; then
+    if [ $(id -u) != 0 ]; then
+      echo "Username: $USER (on $(hostname))" 1>&2
+      sudo ifconfig usb0 up 192.168.2.1 2>/dev/null
+    else
+      ifconfig usb0 up 192.168.2.1 2>/dev/null
+    fi
+  fi
+}
+
+function connect_mac_ethernet() {
+  if [[ "$OSTYPE" == darwin* ]]; then
+    usbif=$(ifconfig | grep -v '^en[01]:' | grep -vw UP | grep -o '^en[0-9]\+')
+    if [ -n "$usbif" ]; then
+      if [ $(id -u) != 0 ]; then
+        echo "Username: $USER (on $(hostname))" 1>&2
+        sudo ifconfig $usbif up 192.168.2.1 2>/dev/null
+      else
+        ifconfig $usbif up 192.168.2.1 2>/dev/null
+      fi
+    fi
+  fi
+}
+
+function connect_ethernet() {
+  retry_count=0
+  while true; do
+    connect_linux_ethernet || true
+    connect_mac_ethernet || true
+    if ping -c 1 -t 1 $TARGET_IPADDR >/dev/null; then break; fi
+    if [[ $retry_count == 0 ]]; then
+      echo "Waiting for Edison to come up at $TARGET_IPADDR.  Connect"
+      echo "a USB cable from this computer to the Edison's USB OTG port."
+    fi
+    sleep 1
+
+    echo -n '.' 1>&2
+    let retry_count=retry_count+1
+    if [[ "$OSTYPE" == darwin* && $retry_count == 3 ]]; then
+      cat <<EOF 1>&2
+
+If the Edison does not appear within 30 seconds of power-on, open
+Network Preferences and look for a new Ethernet device.  Try clicking
+the small + at the bottom of the list of network devices and looking
+for Multifunction Composite Gadget (enX) in the dropdown list.
+Select the new network device.  In the Configure IPv4 dropdown list,
+select Manually, set your IP Address to 192.168.2.1, and click Apply.
+EOF
+    fi
+  done
 }
