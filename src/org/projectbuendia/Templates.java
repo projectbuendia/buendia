766ef430501961d2b7aec489cad624ed678758e0
Pim De Witte <pim@whitespell.com>
2014-12-10 20:11:32 +0000

    better structuring

---
+++ src/org/projectbuendia/Templates.java
@@ -0,0 +1,11 @@
+package org.projectbuendia;
+
+/**
+ * @author Pim de Witte(wwadewitte), Whitespell LLC
+ *         12/10/14
+ *         org.projectbuendia
+ *         ${FILE_NAME}
+ */
+public class Templates {
+
+}
