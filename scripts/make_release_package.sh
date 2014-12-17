06aa309b2856a66d803993972611857283ca1da3
Ka-Ping Yee <ping@zesty.ca>
2014-12-17 01:01:01 +0000

    Update release script to use client-rc and server-rc.

--- scripts/make_release_package.sh
+++ scripts/make_release_package.sh
@@ -17,38 +17,38 @@ cd ..  # root directory is one up from scripts/
 root=$(pwd)
 cd $start
 
-app_build_number=$1
-server_build_number=$2
+client_rc_number=$1
+server_rc_number=$2
 clean_dump_zip=$3
 site=$4
 
 if [ "$1" == "-h" -o -z "$site" ]; then
-  echo "Usage: $0 <app-build-number> <server-build-number> <clean-dump.zip> <site>"
+  echo "Usage: $0 <client-rc-number> <server-rc-number> <clean-dump.zip> <site>"
   echo
-  echo "Constructs a zip file containing the specified builds of the app and"
+  echo "Constructs a zip file containing the specified RC builds of the client"
   echo "the server module, using the specified cleaned database dump and the"
   echo "site-specific initialization SQL for the specified site, together with"
   echo "the scripts needed to set up the Edison.  Example:"
   echo
   echo "    ./make-release-package.sh 456 123 /tmp/dec14-clean.zip kailahun"
   echo
-  echo "This produces buendia-a456-s123-dec14-kailahun.zip; then, to prepare"
+  echo "This produces buendia-c456-s123-dec14-kailahun.zip; then, to prepare"
   echo "a new Edison server, transfer the zip file to a workstation and do:"
   echo
-  echo "    unzip buendia-a456-s123-dec14-kailahun.zip"
-  echo "    cd buendia-a456-s123-dec14-kailahun"
+  echo "    unzip buendia-c456-s123-dec14-kailahun.zip"
+  echo "    cd buendia-c456-s123-dec14-kailahun"
   echo "    ./setup rootpassword GoogleGuest"
   exit 1
 fi
 
 host_port=jenkins.projectbuendia.org:1337
-app_apk=buendia-app_${app_build_number}.apk
-server_omod=buendia-server_${server_build_number}.omod
+client_apk=buendia-client-rc_${client_rc_number}.apk
+server_omod=buendia-server-rc_${server_rc_number}.omod
 site_sql=site_$site.sql
 dump=$(basename $clean_dump_zip)
 dump=${dump%.zip}
 dump=${dump%-clean*}
-package_name=buendia-a${app_build_number}-s${server_build_number}-$dump-$site
+package_name=buendia-c${client_rc_number}-s${server_rc_number}-$dump-$site
 work_dir=/tmp/buendia-package.$$/$package_name
 clean_dump_name=$(basename $clean_dump_zip)
 
@@ -57,11 +57,14 @@ if [ ! -f $root/scripts/$site_sql ]; then
   exit 1
 fi
 
+set -e
 mkdir -p $work_dir
 cp -pr $root/platforms $root/scripts $work_dir
 mkdir $work_dir/omods
-curl -o $work_dir/$app_apk http://$host_port/app/$app_apk
-curl -o $work_dir/omods/$server_omod http://$host_port/server/$server_omod
+curl -o $work_dir/$client_apk http://$host_port/client-rc/$client_apk
+unzip -tq $work_dir/$client_apk
+curl -o $work_dir/omods/$server_omod http://$host_port/server-rc/$server_omod
+unzip -tq $work_dir/omods/$server_omod
 cp $root/modules/xforms-4.3.1.jar $work_dir/omods/xforms-4.3.1.omod
 cp $clean_dump_zip $work_dir/$clean_dump_name
 cp $root/scripts/$site_sql $work_dir/
