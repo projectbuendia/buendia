473ec48f56d8626528791049109ab8c0f1aa6468
Ka-Ping Yee <ping@zesty.ca>
2015-01-17 22:01:13 +0000

    A tidier alphanumericComparator.

--- projectbuendia.openmrs/omod/src/main/java/org/openmrs/projectbuendia/Utils.java
+++ projectbuendia.openmrs/omod/src/main/java/org/openmrs/projectbuendia/Utils.java
@@ -11,11 +11,12 @@ import java.util.regex.Pattern;
 
 public class Utils {
     /**
-     * Compares two objects that may each be Integer or String.  All Integers
-     * sort before all Strings; Integers compare according to their numeric
-     * value and Strings compare according to their string value.
+     * Compares two objects that may each be null, Integer, or String.  null sorts
+     * before everything; all Integers sort before all Strings; Integers sort
+     * according to numeric value; Strings sort according to string value.
      */
     public static Comparator<Object> integerOrStringComparator = new Comparator<Object>() {
+        @Override
         public int compare(Object a, Object b) {
             if (a instanceof Integer && b instanceof Integer) {
                 return (Integer) a - (Integer) b;
@@ -23,43 +24,50 @@ public class Utils {
             if (a instanceof String && b instanceof String) {
                 return ((String) a).compareTo((String) b);
             }
-            return (a instanceof String ? 1 : 0) - (b instanceof String ? 1 : 0);
+            return (a == null ? 0 : a instanceof Integer ? 1 : 2)
+                    - (b == null ? 0 : b instanceof Integer ? 1 : 2);
         }
     };
 
     /**
-     * Compares two lists, whose elements may be Integers or Strings,
+     * Compares two lists, each of whose elements is a null, Integer, or String,
      * lexicographically by element, just like Python does.
      */
     public static Comparator<List<Object>> integerOrStringListComparator = new Comparator<List<Object>>() {
+        @Override
         public int compare(List<Object> a, List<Object> b) {
-            int result = 0;
-            for (int i = 0; result == 0; i++) {
-                if (i >= a.size() || i >= b.size()) {
-                    return a.size() - b.size();
+            for (int i = 0; i < Math.min(a.size(), b.size()); i++) {
+                int result = integerOrStringComparator.compare(a.get(i), b.get(i));
+                if (result != 0) {
+                    return result;
                 }
-                result = integerOrStringComparator.compare(a.get(i), b.get(i));
             }
-            return result;
+            return a.size() - b.size();
         }
     };
 
     /**
      * Compares two strings in a way that sorts alphabetic parts in alphabetic
-     * order and numeric parts in numeric order, returning -1, 0, or 1.
+     * order and numeric parts in numeric order, while guaranteeing that:
+     *   - compare(s, t) == 0 if and only if s.equals(t).
+     *   - compare(s, s + t) < 0 for any strings s and t.
+     *   - compare(s + x, s + y) == Integer.compare(x, y) for all integers x, y
+     *     and strings s that do not end in a digit.
+     *   - compare(s + t, s + u) == compare(s, t) for all strings s and strings
+     *     t, u that consist entirely of Unicode letters.
      * For example, the strings ["b1", "a11a", "a11", "a2", "a2b", "a2a", "a1"]
      * have the sort order ["a1", "a2", "a2a", "a2b", "a11", "a11a", "b1"].
      */
     public static Comparator<String> alphanumericComparator = new Comparator<String>() {
+        // Note: Use of \L here assumes a string that is already NFC-normalized.
+        private final Pattern NUMBER_OR_WORD_PATTERN = Pattern.compile("[0-9]+|\\p{L}+");
+
         /**
-         * Breaks a string into an array of Numbers (from sequences of ASCII digits)
-         * and Strings (from sequences of Unicode letters after NFC normalization).
-         * Other characters are ignored.
+         * Breaks a string into a list of Integers (from sequences of ASCII digits)
+         * and Strings (from sequences of letters).  Other characters are ignored.
          */
         private List<Object> getParts(String str) {
-            Pattern numberOrWord = Pattern.compile("[0-9]+|\\p{L}+");
-            Matcher matcher = numberOrWord.matcher(
-                    str == null ? "" : Normalizer.normalize(str, Normalizer.Form.NFC));
+            Matcher matcher = NUMBER_OR_WORD_PATTERN.matcher(str);
             List<Object> parts = new ArrayList<>();
             while (matcher.find()) {
                 String part = matcher.group();
@@ -68,13 +76,23 @@ public class Utils {
             return parts;
         }
 
+        @Override
         public int compare(String a, String b) {
-            List<Object> aParts = getParts(a);
-            List<Object> bParts = getParts(b);
-            // for breaking ties, e.g. "a04b" < "a4b"
-            aParts.add("");
+            String aNormalized = Normalizer.normalize(a == null ? "" : a, Normalizer.Form.NFC);
+            String bNormalized = Normalizer.normalize(b == null ? "" : b, Normalizer.Form.NFC);
+            List<Object> aParts = getParts(aNormalized);
+            List<Object> bParts = getParts(bNormalized);
+            // Add a separator to ensure that the tiebreakers added below are never
+            // compared against the actual numeric or alphabetic parts.
+            aParts.add(null);
+            bParts.add(null);
+            // Break ties between strings that yield the same parts (e.g. "a04b"
+            // and "a4b") using the normalized original string as a tiebreaker.
+            aParts.add(aNormalized);
+            bParts.add(bNormalized);
+            // Break ties between strings that become the same after normalization
+            // using the non-normalized string as a further tiebreaker.
             aParts.add(a);
-            bParts.add("");
             bParts.add(b);
             return integerOrStringListComparator.compare(aParts, bParts);
         }
--- projectbuendia.openmrs/omod/src/test/java/org/openmrs/projectbuendia/UtilsTest.java
+++ projectbuendia.openmrs/omod/src/test/java/org/openmrs/projectbuendia/UtilsTest.java
@@ -35,8 +35,8 @@ public class UtilsTest {
     public void testAlphanumericComparator() throws Exception {
         String[] elements = {"b1", "a11a", "a11", "a2", "a2b", "a02b", "a2a", "a1"};
         String[] sorted = elements.clone();
-        String[] expected = {"a1", "a2", "a2a", "a02b", "a2b", "a11", "a11a", "b1"};
         Arrays.sort(sorted, Utils.alphanumericComparator);
+        String[] expected = {"a1", "a2", "a2a", "a02b", "a2b", "a11", "a11a", "b1"};
         assertArrayEquals(expected, sorted);
     }
 }
