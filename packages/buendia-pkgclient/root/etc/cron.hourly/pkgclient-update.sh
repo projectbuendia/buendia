291182dd3ec6287576a10aa2e0d0e93b1b7eafe8
Ka-Ping Yee <ping@zesty.ca>
2015-01-22 03:59:40 +0000

    Allow unauthenticated packages.

--- packages/buendia-pkgclient/root/etc/cron.hourly/pkgclient-update.sh
+++ packages/buendia-pkgclient/root/etc/cron.hourly/pkgclient-update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . /usr/share/buendia/config.d/pkgclient
 
-apt-get -qqy install $(cat /usr/share/buendia/packages.list.d/*)
+apt-get -qqy --allow-unauthenticated install $(cat /usr/share/buendia/packages.list.d/*)
