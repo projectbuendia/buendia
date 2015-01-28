4c4013ddf04c239a4442511756e519b7e55514c5
mslatour <sanderlatour@gmail.com>
2015-01-28 21:42:25 +0000

    Cleaned up, fixed condition check

--- packages/buendia-tomcat7/root/usr/bin/buendia-tomcat7-watchdog.sh
+++ packages/buendia-tomcat7/root/usr/bin/buendia-tomcat7-watchdog.sh
@@ -2,7 +2,7 @@
 
 CATALINA_PID=/var/run/tomcat7.pid
 
-if service tomcat7 status > /dev/null 2>&1 ; then
+if ! service tomcat7 status > /dev/null 2>&1 ; then
 	# Ensure we keep timestamps
 	cp="cp --preserve=timestamps"
 	tar="tar --atime-preserve"
@@ -14,24 +14,22 @@ if service tomcat7 status > /dev/null 2>&1 ; then
 	# Notify that we are dumping
 	now=$(date); echo "$now >> Tomcat7 is not running, dumping to $DUMPDIR"
 
-	# Store the datetime information
-	date > $DUMPDIR/datetime
 	# Store the PID file, if we have one
 	if [ -f "$CATALINA_PID" ]; then
-		$cp $CATALINA_PID $DUMPDIR/tomcat7.pid
+		$cp $CATALINA_PID $DUMPDIR/tomcat7.pid > /dev/null
 	fi
 	# Save a copy of the tomcat logs
-	$tar -czf $DUMPDIR/tomcat7_logs.tar.gz /var/log/tomcat7/*
+	$tar -czf $DUMPDIR/tomcat7_logs.tar.gz /var/log/tomcat7/* > /dev/null
 	# Save a copy of the tomcat files
-	$tar -czf $DUMPDIR/tomcat7_files.tar.gz /usr/share/tomcat7/*
+	$tar -czf $DUMPDIR/tomcat7_files.tar.gz /usr/share/tomcat7/* > /dev/null
 	# Save a copy of the syslog
-	$tar -czf $DUMPDIR/syslog.tar.gz /var/log/syslog
+	$tar -czf $DUMPDIR/syslog.tar.gz /var/log/syslog > /dev/null
 
 	# Try to restart tomcat
 	now=$(date); echo "Restarting tomcat7..."
 	if service tomcat7 restart; then
 		now=$(date); echo "$now >> Tomcat7 has been restarted."
-	else:
+	else
 		now=$(date); echo "$now >> Tomcat7 could not be restarted."
 	fi
 fi
