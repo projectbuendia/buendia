cdb4e4d094e41857b1d444ca6f328173060a0908
mslatour <sanderlatour@gmail.com>
2015-01-28 17:47:56 +0000

    Added a tomcat7 watchdog that stores a dump and attempts a restart when tomcat is not running

---
+++ packages/buendia-tomcat7/root/usr/bin/buendia-tomcat7-watchdog.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+CATALINE_PID=/var/run/tomcat7.pid
+
+if service tomcat7 status > /dev/null 2>&1 ; then
+	# Ensure we keep timestamps
+	cp="cp --preserve=timestamps"
+	tar="tar --atime-preserve"
+
+	# Create directory to store dump
+	mkdir -p /var/log/.tomcat_watchdog_dumps/
+	DUMPDIR=$(mktemp -d /var/log/.tomcat_watchdog/dump.XXXXXXXXXX)
+
+	# Notify that we are dumping
+	now=$(date); echo "$now >> Tomcat7 is not running, dumping to $DUMPDIR"
+
+	# Store the datetime information
+	date > $DUMPDIR/datetime
+	# Store the PID file, if we have one
+	if [ -f "$CATALINA_PID" ]; then
+		$cp $CATALINA_PID $DUMPDIR/tomcat7.pid
+	fi
+	# Save a copy of the tomcat logs
+	$tar -czf $DUMPDIR/tomcat7_logs.tar.gz /var/log/tomcat7/*
+	# Save a copy of the tomcat files
+	$tar -czf $DUMPDIR/tomcat7_files.tar.gz /usr/share/tomcat7/*
+	# Save a copy of the syslog
+	$tar -czf $DUMPDIR/syslog.tar.gz /var/log/syslog
+
+	# Try to restart tomcat
+	now=$(date); echo "Restarting tomcat7..."
+	if service tomcat7 restart; then
+		now=$(date); echo "$now >> Tomcat7 has been restarted."
+	else:
+		now=$(date); echo "$now >> Tomcat7 could not be restarted."
+	fi
+fi
+
