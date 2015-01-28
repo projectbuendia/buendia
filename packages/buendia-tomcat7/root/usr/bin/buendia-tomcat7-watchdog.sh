02df8cda1daa0ed0e2ab2276821d0e0ba9972ff7
mslatour <sanderlatour@gmail.com>
2015-01-28 20:45:52 +0000

    Fixed up some sloppy mistakes, changed the name of the directory. Will try it out now.

--- packages/buendia-tomcat7/root/usr/bin/buendia-tomcat7-watchdog.sh
+++ packages/buendia-tomcat7/root/usr/bin/buendia-tomcat7-watchdog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CATALINE_PID=/var/run/tomcat7.pid
+CATALINA_PID=/var/run/tomcat7.pid
 
 if service tomcat7 status > /dev/null 2>&1 ; then
 	# Ensure we keep timestamps
@@ -8,8 +8,8 @@ if service tomcat7 status > /dev/null 2>&1 ; then
 	tar="tar --atime-preserve"
 
 	# Create directory to store dump
-	mkdir -p /var/log/.tomcat_watchdog_dumps/
-	DUMPDIR=$(mktemp -d /var/log/.tomcat_watchdog/dump.XXXXXXXXXX)
+	DUMPDIR=/var/log/tomcat_watchdog/$(date +"%Y-%m-%d.%H.%M")
+	mkdir -p $DUMPDIR
 
 	# Notify that we are dumping
 	now=$(date); echo "$now >> Tomcat7 is not running, dumping to $DUMPDIR"
