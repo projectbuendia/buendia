12267b51cf8a3b9766ecc2566fb93f23aadc331a
Ka-Ping Yee <ping@zesty.ca>
2015-03-24 03:11:11 +0000

    Replace tabs with spaces and adjust whitespace formatting.

--- projectbuendia.openmrs/omod/src/main/java/org/projectbuendia/openmrs/web/controller/ProjectBuendiaManageController.java
+++ projectbuendia.openmrs/omod/src/main/java/org/projectbuendia/openmrs/web/controller/ProjectBuendiaManageController.java
@@ -6,13 +6,11 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * The main controller.
- */
+/** The main controller. */
 @Controller
 public class ProjectBuendiaManageController {
-	@RequestMapping(value = "/module/projectbuendia/openmrs/manage", method = RequestMethod.GET)
-	public void manage(ModelMap model) {
-		model.addAttribute("user", Context.getAuthenticatedUser());
-	}
+    @RequestMapping(value = "/module/projectbuendia/openmrs/manage", method = RequestMethod.GET)
+    public void manage(ModelMap model) {
+        model.addAttribute("user", Context.getAuthenticatedUser());
+    }
 }
