254b560db31781db530f15ae93df1245fb3de0ee
akalachman <akalachman@gmail.com>
2015-03-31 17:58:21 +0100

    Finishes(?) licensing for the Buendia repo with exception of BuendiaXFormBuilder*, adding standalone AUTHORS/CONTRIBUTORS/CONTRIBUTING and adding licenses to relevant files missing them. Some files (Maven config, certain XML resources, service conf definitions) are intentionally left out

--- projectbuendia.openmrs/api/src/test/java/org/projectbuendia/openmrs/api/ProjectBuendiaServiceTest.java
+++ projectbuendia.openmrs/api/src/test/java/org/projectbuendia/openmrs/api/ProjectBuendiaServiceTest.java
@@ -1,3 +1,14 @@
+// Copyright 2015 The Project Buendia Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License.  You may obtain a copy
+// of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distrib-
+// uted under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+// OR CONDITIONS OF ANY KIND, either express or implied.  See the License for
+// specific language governing permissions and limitations under the License.
+
 package org.projectbuendia.openmrs.api;
 
 import static org.junit.Assert.*;
@@ -8,7 +19,7 @@ import org.openmrs.test.BaseModuleContextSensitiveTest;
 import org.projectbuendia.openmrs.api.ProjectBuendiaService;
 
 /**
- * Tests {@link ${ProjectBuendiaService}}.
+ * Tests {@link ProjectBuendiaService}.
  */
 public class ProjectBuendiaServiceTest extends BaseModuleContextSensitiveTest {
 	
