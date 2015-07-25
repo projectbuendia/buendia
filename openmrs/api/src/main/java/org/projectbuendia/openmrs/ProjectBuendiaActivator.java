96de0a2563c2568227e4c034508ac1ba315d507b
Ka-Ping Yee <ping@zesty.ca>
2015-07-25 04:32:00 -0700

    Tidy.

--- openmrs/api/src/main/java/org/projectbuendia/openmrs/ProjectBuendiaActivator.java
+++ openmrs/api/src/main/java/org/projectbuendia/openmrs/ProjectBuendiaActivator.java
@@ -11,57 +11,36 @@
 
 package org.projectbuendia.openmrs;
 
-import org.apache.commons.logging.Log; 
+import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.module.ModuleActivator;
 
-/**
- * This class contains the logic that is run every time this module is either started or stopped.
- */
+/** Logic that is run every time this module is either started or stopped. */
 public class ProjectBuendiaActivator implements ModuleActivator {
-	
-	protected Log log = LogFactory.getLog(getClass());
-		
-	/**
-	 * @see ModuleActivator#willRefreshContext()
-	 */
-	public void willRefreshContext() {
-		log.info("Refreshing Project Buendia Module");
-	}
-	
-	/**
-	 * @see ModuleActivator#contextRefreshed()
-	 */
-	public void contextRefreshed() {
-		log.info("Project Buendia Module refreshed");
-	}
-	
-	/**
-	 * @see ModuleActivator#willStart()
-	 */
-	public void willStart() {
-		log.info("Starting Project Buendia Module");
-	}
-	
-	/**
-	 * @see ModuleActivator#started()
-	 */
-	public void started() {
-		log.info("Project Buendia Module started");
-	}
-	
-	/**
-	 * @see ModuleActivator#willStop()
-	 */
-	public void willStop() {
-		log.info("Stopping Project Buendia Module");
-	}
-	
-	/**
-	 * @see ModuleActivator#stopped()
-	 */
-	public void stopped() {
-		log.info("Project Buendia Module stopped");
-	}
-		
+
+    protected Log log = LogFactory.getLog(getClass());
+
+    public void willRefreshContext() {
+        log.info("Refreshing Project Buendia module");
+    }
+
+    public void contextRefreshed() {
+        log.info("Project Buendia module refreshed");
+    }
+
+    public void willStart() {
+        log.info("Starting Project Buendia module");
+    }
+
+    public void started() {
+        log.info("Project Buendia module started");
+    }
+
+    public void willStop() {
+        log.info("Stopping Project Buendia module");
+    }
+
+    public void stopped() {
+        log.info("Project Buendia module stopped");
+    }
 }
