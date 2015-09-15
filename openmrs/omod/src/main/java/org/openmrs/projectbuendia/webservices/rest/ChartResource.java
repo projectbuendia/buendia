cc06231e63f27033fc8c17063adff30ece9a5156
Ka-Ping Yee <ping@zesty.ca>
2015-09-14 23:40:09 -0700

    Import additional information about charts from CSV (required, format, caption format, script) and return charts in a new JSON format from the server.

--- openmrs/omod/src/main/java/org/openmrs/projectbuendia/webservices/rest/ChartResource.java
+++ openmrs/omod/src/main/java/org/openmrs/projectbuendia/webservices/rest/ChartResource.java
@@ -11,9 +11,12 @@
 
 package org.openmrs.projectbuendia.webservices.rest;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.openmrs.Concept;
+import org.openmrs.Field;
 import org.openmrs.Form;
 import org.openmrs.FormField;
+import org.openmrs.api.ConceptService;
 import org.openmrs.api.FormService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.webservices.rest.SimpleObject;
@@ -24,10 +27,16 @@ import org.openmrs.module.webservices.rest.web.response.ResponseException;
 import org.openmrs.util.FormUtil;
 import org.projectbuendia.openmrs.webservices.rest.RestController;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-import java.util.TreeMap;
+import java.util.Map;
+import java.util.SortedMap;
 import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * REST resource for charts. These are stored as OpenMRS forms, but that's
@@ -37,15 +46,14 @@ import java.util.TreeSet;
 @Resource(name = RestController.REST_VERSION_1_AND_NAMESPACE + "/chart",
     supportedClass = Form.class, supportedOpenmrsVersions = "1.10.*,1.11.*")
 public class ChartResource extends AbstractReadOnlyResource<Form> {
-    private static final String GROUPS = "groups";
-    private static final String VERSION = "version";
-    private static final String CONCEPTS = "concepts";
-
+    private static final Pattern COMPRESSIBLE_UUID = Pattern.compile("^([0-9]+)A+$");
     private final FormService formService;
+    private final ConceptService conceptService;
 
     public ChartResource() {
         super("chart", Representation.DEFAULT, Representation.FULL);
         formService = Context.getFormService();
+        conceptService = Context.getConceptService();
     }
 
     /**
@@ -60,53 +68,94 @@ public class ChartResource extends AbstractReadOnlyResource<Form> {
     }
 
     /**
-     * Adds the following fields to the {@link SimpleObject}:
-     * <ul>
-     * <li>"version": the version number (e.g. 0.2.3) of the form
-     * </ul>
-     * <p/>
-     * <p>If details are requested with the URL query parameter "?v=full",
-     * also adds the following fields to the {@link SimpleObject}:
-     * <ul>
-     * <li>"groups": a {@link List} of {@link SimpleObject}s, each containing:
-     * <ul>
-     * <li>"uuid": the UUID for the concept representing the group (note: when
-     * defining groups in OpenMRS for charts returned by this endpoint,
-     * each group MUST be represented by a concept or this will fail)
-     * <li>"concepts": a {@link List} of UUIDs of the concepts in the group
-     * </ul>
-     * </ul>
+     * Populates the {@link SimpleObject} with 'version' (the version number of the form)
+     * and, if details are requested with the query parameter "?v=full", adds the list of
+     * chart sections in 'sections' containing the details of each tile or row in the chart.
      * @param context the request context; specify the URL query parameter
      *                "?v=full" to get a list of the groups and concepts in the form.
      */
     @Override
     protected void populateJsonProperties(Form form, RequestContext context, SimpleObject json,
                                           long snapshotTime) {
-        json.put(VERSION, form.getVersion());
+        json.put("version", form.getVersion());
         if (context.getRepresentation() != Representation.FULL) return;
-        List<SimpleObject> groups = new ArrayList<>();
-        TreeMap<Integer, TreeSet<FormField>> formStructure = FormUtil.getFormStructure(form);
-        for (FormField groupField : formStructure.get(0)) {
-            Concept groupConcept = groupField.getField().getConcept();
-            if (groupConcept == null) {
-                throw new ConfigurationException("Chart %s has non-concept top-level field %s",
-                    form.getUuid(), groupField.getField().getName());
+
+        List<Map> sections = new ArrayList<>();
+        SortedMap<Integer, TreeSet<FormField>> structure = FormUtil.getFormStructure(form);
+        for (FormField sectionFormField : structure.get(0)) {
+            Field sectionField = sectionFormField.getField();
+            Map<String, Object> section = parseFieldDescription(sectionField);
+            section.put("label", sectionField.getName());
+            List<Map> items = new ArrayList<>();
+            for (FormField itemFormField : structure.get(sectionFormField.getId())) {
+                Field itemField = itemFormField.getField();
+                Map<String, Object> item = parseFieldDescription(itemField);
+                item.put("label", itemField.getName());
+                item.put("concepts", getConceptUuids(item.get("concept"), itemField));
+                item.remove("concept");
+                items.add(item);
             }
-            SimpleObject group = new SimpleObject();
-            group.put("uuid", groupConcept.getUuid());
-            List<String> groupFieldConceptIds = new ArrayList<>();
-            for (FormField fieldInGroup : formStructure.get(groupField.getId())) {
-                Concept fieldConcept = fieldInGroup.getField().getConcept();
-                if (fieldConcept == null) {
-                    throw new ConfigurationException("Chart %s has non-concept subfield %s",
-                        form.getUuid(), fieldInGroup.getField().getName());
-                }
-                groupFieldConceptIds.add(fieldConcept.getUuid());
-            }
-            group.put(CONCEPTS, groupFieldConceptIds);
-            groups.add(group);
+            section.put("items", items);
+            sections.add(section);
         }
-        json.put(GROUPS, groups);
+        json.put("sections", sections);
+    }
+
+    /**
+     * Returns a list of compressed concept UUIDs (see compressUuid), given the "concepts"
+     * member of a field's description JSON.  If the "concepts" members is missing, returns
+     * the field's concept.  The description is assumed to be correctly formatted, i.e. the
+     * concepts argument should be a list of integer IDs of existing concepts.  Otherwise, an
+     * uncaught exception will be thrown (as with any other case of internal database corruption).
+     */
+    private List<Object> getConceptUuids(Object concepts, Field field) {
+        if (concepts == null) {
+            return Collections.singletonList(compressUuid(field.getConcept().getUuid()));
+        }
+        // If the casts in this method fail, we want it to produce an uncaught exception and
+        // HTTP error 500 (not error 4xx, which is for invalid user input).
+        List conceptIds = (List) concepts;
+        List<Object> result = new ArrayList<>();
+        for (Object item : conceptIds) {
+            int id = (Integer) item;
+            Concept concept = conceptService.getConcept(id);
+            if (concept == null) {
+                throw new IllegalStateException("Concept " + id + " not found");
+            }
+            result.add(compressUuid(concept.getUuid()));
+        }
+        return result;
+    }
+
+    /**
+     * Saves a bit of JSON space by using integers to represent UUIDs that are exactly 36
+     * consist of an integer followed by a string of "A"s.  All other UUIDs are left as strings.
+     */
+    private static Object compressUuid(String uuid) {
+        Matcher matcher = COMPRESSIBLE_UUID.matcher(uuid);
+        if (uuid.length() == 36 && matcher.matches()) {
+            return Integer.valueOf(matcher.group(1));
+        }
+        return uuid;
+    }
+
+    private static Map<String, Object> parseFieldDescription(Field field) {
+        String description = field.getDescription().trim();
+        if (description.startsWith("{\"")) {
+            try {
+                Map<String, Object> config =
+                    new ObjectMapper().readValue(field.getDescription(), Map.class);
+                for (String key : config.keySet()) {
+                    if (config.get(key) == null || "".equals(config.get(key))) {
+                        config.remove(key);
+                    }
+                }
+                return config;
+            } catch (IOException e) {
+                throw new InvalidObjectDataException(
+                    "Invalid JSON in description of field " + field.getId());
+            }
+        } else return new HashMap<String, Object>();
     }
 
     /**
