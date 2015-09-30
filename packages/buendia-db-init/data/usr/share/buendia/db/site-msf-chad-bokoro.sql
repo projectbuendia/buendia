020491f996316587408e855ad0d1a721c3510707
Dan Cunningham <daniel.cunningham@gmail.com>
2015-09-30 16:53:14 +0100

    Chad database script using UUIDs

--- packages/buendia-db-init/data/usr/share/buendia/db/site-msf-chad-bokoro.sql
+++ packages/buendia-db-init/data/usr/share/buendia/db/site-msf-chad-bokoro.sql
@@ -1,6 +1,6 @@
 /*
  * This script takes a database cleared by clear_server.sql and sets up
- * initial data for the MSF EMC in Magburaka, Sierra Leone.  It will
+ * initial data for the MSF Nutrition Programme in Bokoro, Chad.  It will
  * also double checks some data that should already be present and adds
  * it if missing.
  *
@@ -22,29 +22,36 @@ UPDATE global_property SET property_value = 'en, en_GB_client'
 
 -- Make sure camp and zone locations are present.
 -- ON DUPLICATE IGNORE is safe as there is a unique index on uuid.
--- DC: Unsure if this will work setting location_id and not setting uuid.
-SET @emc_id = 'msf.loc.chad';
-
-INSERT INTO location (location_id, name, creator, date_created) VALUES
-    (@emc_id, 'Chad', @admin_id, NOW())
+INSERT INTO location (name, creator, date_created, uuid) VALUES
+    ('Chad', @admin_id, NOW(), 'c149ee48-bcda-4661-a3bf-d98847dfd18c')
     ON DUPLICATE KEY UPDATE uuid = uuid;
 
-SET @itfc_id = 'msf.loc.chad.bokoro.itfc';
+SELECT @emc_id := location_id FROM location
+    WHERE uuid = 'c149ee48-bcda-4661-a3bf-d98847dfd18c';
 
-INSERT INTO location (location_id, name, creator, date_created, parent_location) VALUES
-    (@itfc_id, 'Bokoro ITFC', @admin_id, NOW(), @emc_id),
-    ('msf.loc.chad.bokoro.bokoro-atfc', 'Bokoro ATFC', @admin_id, NOW(), @emc_id),
-    ('msf.loc.chad.bokoro.ngama-atfc', 'Ngama ATFC', @admin_id, NOW(), @emc_id),
-    ('msf.loc.chad.bokoro.moyto-atfc', 'Moyto ATFC', @admin_id, NOW(), @emc_id)
+INSERT INTO location (name, creator, date_created, uuid, parent_location) VALUES
+    ('Bokoro ITFC', @admin_id, NOW(), '69890fb4-49bb-4c14-a834-b1dead6a34df', @emc_id),
+    ('Bokoro ATFC', @admin_id, NOW(), 'e8f112b2-e121-43b3-b1cc-d5f16a88e567', @emc_id),
+    ('Ngama ATFC', @admin_id, NOW(), '0c96ad58-f580-4f0a-88ba-cafb88b406a8', @emc_id),
+    ('Moyto ATFC', @admin_id, NOW(), 'e8c01a9c-1cb1-43dc-9478-4bcada286bde', @emc_id)
     ON DUPLICATE KEY UPDATE uuid = uuid;
 
 -- Add the tents.
-INSERT INTO location (location_id, name, creator, date_created, parent_location) VALUES
-    ('ITFC ICU', @admin_id, NOW(), @itfc_id),
-    ('ITFC Phase I', @admin_id, NOW(), @itfc_id),
-    ('ITFC Phase I-T', @admin_id, NOW(), @itfc_id),
-    ('ITFC Phase II', @admin_id, NOW(), @itfc_id),
-    ('Bokoro ATFC', @admin_id, NOW(), 'msf.loc.chad.bokoro.bokoro-atfc'),
-    ('Ngama ATFC', @admin_id, NOW(), 'msf.loc.chad.bokoro.ngama-atfc'),
-    ('Moyto ATFC', @admin_id, NOW(), 'msf.loc.chad.bokoro.moyto-atfc')
+SELECT @bokoro_itfc_id := location_id FROM location
+    WHERE uuid='69890fb4-49bb-4c14-a834-b1dead6a34df';
+SELECT @bokoro_atfc_id := location_id FROM location
+    WHERE uuid='e8f112b2-e121-43b3-b1cc-d5f16a88e567';
+SELECT @ngama_atfc_id := location_id FROM location
+    WHERE uuid = '0c96ad58-f580-4f0a-88ba-cafb88b406a8';
+SELECT @moyto_atfc_id := location_id FROM location
+    WHERE uuid = 'e8c01a9c-1cb1-43dc-9478-4bcada286bde';
+
+INSERT INTO location (name, creator, date_created, uuid, parent_location) VALUES
+    ('ITFC ICU', @admin_id, NOW(), '03213d10-550f-4253-bf66-d7c6ec8d1eb5', @bokoro_itfc_id),
+    ('ITFC Phase I', @admin_id, NOW(), '3a15c457-ef0d-4129-8f61-893b084977fa', @bokoro_itfc_id),
+    ('ITFC Phase I-T', @admin_id, NOW(), '59964e20-bbb9-4c5e-8a0b-a2d7b656362d', @bokoro_itfc_id),
+    ('ITFC Phase II', @admin_id, NOW(), '0c3740e1-a204-4f2b-bceb-97a1334bd653', @bokoro_itfc_id),
+    ('Bokoro ATFC', @admin_id, NOW(), '3d3da080-2493-4733-93f2-5fcc1faa3403', @bokoro_atfc_id),
+    ('Ngama ATFC', @admin_id, NOW(), '3086db2b-50ad-4bfb-b7eb-19f904660ca1', @ngama_atfc_id),
+    ('Moyto ATFC', @admin_id, NOW(), '0a01b40c-bc74-4910-8d56-b7a408af4aff', @moyto_atfc_id)
     ON DUPLICATE KEY UPDATE uuid = uuid;
