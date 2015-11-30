03abde24563ce76c0f40939a3b102e95ada36341
Fabian Tamp <fabian.tamp@gmail.com>
2015-11-30 14:19:13 +0800

    Genericise paginated sync logic for patients.
    This will allow us to reuse the same code for paginated sync of observations and orders.

--- openmrs/api/src/main/java/org/projectbuendia/openmrs/api/db/hibernate/HibernateProjectBuendiaDAO.java
+++ openmrs/api/src/main/java/org/projectbuendia/openmrs/api/db/hibernate/HibernateProjectBuendiaDAO.java
@@ -19,13 +19,15 @@ import org.hibernate.classic.Session;
 import org.hibernate.criterion.Disjunction;
 import org.hibernate.type.StandardBasicTypes;
 import org.hibernate.type.Type;
+import org.openmrs.BaseOpenmrsData;
 import org.openmrs.Obs;
 import org.openmrs.Order;
 import org.openmrs.Patient;
-import org.projectbuendia.openmrs.PatientSyncParameters;
+import org.projectbuendia.openmrs.sync.PatientSyncParameters;
 import org.projectbuendia.openmrs.api.SyncToken;
 import org.projectbuendia.openmrs.api.db.ProjectBuendiaDAO;
 import org.projectbuendia.openmrs.api.db.SyncPage;
+import org.projectbuendia.openmrs.sync.SyncParameters;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
@@ -77,50 +79,10 @@ public class HibernateProjectBuendiaDAO implements ProjectBuendiaDAO {
     @Override
     public SyncPage<Patient> getPatientsModifiedAfter(
             @Nullable SyncToken syncToken, boolean includeVoided, int maxResults) {
-        Session session = sessionFactory.getCurrentSession();
-
-        Criteria criteria = session.createCriteria(PatientSyncParameters.class);
-
-        if (syncToken != null) {
-            // (a, b) > (x, y) is equivalent to (a > x) OR ((a = x) AND (b > y)). See
-            // http://dev.mysql.com/doc/refman/5.7/en/comparison-operators.html#operator_greater-than
-            criteria.add(sqlRestriction(
-                    // {alias} is substituted for the table alias that hibernate uses for the type.
-                    "({alias}.date_updated, {alias}.uuid) > (?, ?)",
-                    new Object[]{
-                            syncToken.greaterThanOrEqualToTimestamp,
-                            // If syncToken.greaterThanUuid is null, we use the empty string, which
-                            // is 'smaller' than every other string in terms of sort order.
-                            syncToken.greaterThanUuid == null ? "" : syncToken.greaterThanUuid},
-                    new Type[] {StandardBasicTypes.TIMESTAMP, StandardBasicTypes.STRING}));
-        }
-
-        if (!includeVoided) {
-            criteria.createCriteria("patient").add(eq("personVoided", false));
-        }
-
-        criteria.addOrder(asc("dateUpdated"))
-                .addOrder(asc("uuid"));
-
-        if (maxResults > 0) {
-            criteria.setMaxResults(maxResults);
-        }
-
         //noinspection unchecked
-        List<PatientSyncParameters> dbList = criteria.list();
-
-        // PatientSyncParameters --> Patient
-        ArrayList<Patient> patients = new ArrayList<>(dbList.size());
-        for (PatientSyncParameters params : dbList) {
-            patients.add(params.getPatient());
-        }
-
-        SyncToken token = null;
-        if (dbList.size() > 0) {
-            PatientSyncParameters lastEntry = dbList.get(dbList.size() - 1);
-            token = new SyncToken(lastEntry.getDateUpdated(), lastEntry.getUuid());
-        }
-        return new SyncPage<>(patients, token);
+        return fetchSyncPage(
+                (Class<SyncParameters<Patient>>) (Class<?>) PatientSyncParameters.class,
+                syncToken, includeVoided, maxResults);
     }
 
     @Override
@@ -140,5 +102,67 @@ public class HibernateProjectBuendiaDAO implements ProjectBuendiaDAO {
         }
         //noinspection unchecked
         return criteria.list();
+    }
+
+
+    private <T extends BaseOpenmrsData> SyncPage<T> fetchSyncPage(
+            Class<SyncParameters<T>> clazz, @Nullable SyncToken syncToken,
+            boolean includeVoided, int maxResults) {
+        List<SyncParameters<T>> dbList =
+                fetchResults(clazz, syncToken, includeVoided, maxResults);
+        return resultsToSyncPage(dbList);
+
+
+    }
+
+    private <T extends SyncParameters> List<T> fetchResults(
+            Class<T> clazz, @Nullable SyncToken syncToken, boolean includeVoided, int maxResults) {
+        Session session = sessionFactory.getCurrentSession();
+
+        Criteria criteria = session.createCriteria(clazz);
+
+        if (syncToken != null) {
+            // (a, b) > (x, y) is equivalent to (a > x) OR ((a = x) AND (b > y)). See
+            // http://dev.mysql.com/doc/refman/5.7/en/comparison-operators.html#operator_greater-than
+            criteria.add(sqlRestriction(
+                    // {alias} is substituted for the table alias that hibernate uses for the type.
+                    "({alias}.date_updated, {alias}.uuid) > (?, ?)",
+                    new Object[]{
+                            syncToken.greaterThanOrEqualToTimestamp,
+                            // If syncToken.greaterThanUuid is null, we use the empty string, which
+                            // is 'smaller' than every other string in terms of sort order.
+                            syncToken.greaterThanUuid == null ? "" : syncToken.greaterThanUuid},
+                    new Type[] {StandardBasicTypes.TIMESTAMP, StandardBasicTypes.STRING}));
+        }
+
+        if (!includeVoided) {
+            criteria.createCriteria("item").add(eq("voided", false));
+        }
+
+        criteria.addOrder(asc("dateUpdated"))
+                .addOrder(asc("uuid"));
+
+        if (maxResults > 0) {
+            criteria.setMaxResults(maxResults);
+        }
+
+        //noinspection unchecked
+        return criteria.list();
+    }
+
+    private <T extends BaseOpenmrsData> SyncPage<T> resultsToSyncPage(
+            List<SyncParameters<T>> dbResults) {
+        // PatientSyncParameters --> Patient
+        ArrayList<T> items = new ArrayList<>(dbResults.size());
+        for (SyncParameters<T> params : dbResults) {
+            items.add(params.getItem());
+        }
+
+        SyncToken token = null;
+        if (dbResults.size() > 0) {
+            SyncParameters<T> lastEntry = dbResults.get(dbResults.size() - 1);
+            token = new SyncToken(lastEntry.getDateUpdated(), lastEntry.getUuid());
+        }
+        return new SyncPage<>(items, token);
     }
 }
