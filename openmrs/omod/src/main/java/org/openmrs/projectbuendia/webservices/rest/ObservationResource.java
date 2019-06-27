ed89b286c73a9a560260035e19e68957f3b05e53
Ka-Ping Yee <zestyping@gmail.com>
2019-06-27 02:06:29 -0700

    Revert "Server-side changes required to support notes panel."
    This reverts commit c01b26ce1b1f1a5bbdf2d9601b7fdb922856b834.

--- openmrs/omod/src/main/java/org/openmrs/projectbuendia/webservices/rest/ObservationResource.java
+++ openmrs/omod/src/main/java/org/openmrs/projectbuendia/webservices/rest/ObservationResource.java
@@ -14,6 +14,7 @@
 package org.openmrs.projectbuendia.webservices.rest;
 
 import org.openmrs.Obs;
+import org.openmrs.Provider;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.webservices.rest.SimpleObject;
 import org.openmrs.module.webservices.rest.web.RequestContext;
@@ -22,6 +23,7 @@ import org.openmrs.module.webservices.rest.web.annotation.Resource;
 import org.openmrs.module.webservices.rest.web.resource.api.Listable;
 import org.openmrs.module.webservices.rest.web.resource.api.Searchable;
 import org.openmrs.module.webservices.rest.web.response.ResponseException;
+import org.openmrs.projectbuendia.Utils;
 import org.projectbuendia.openmrs.api.ProjectBuendiaService;
 import org.projectbuendia.openmrs.api.SyncToken;
 import org.projectbuendia.openmrs.api.db.SyncPage;
@@ -68,13 +70,47 @@ public class ObservationResource implements Listable, Searchable {
                         syncFrom, syncFrom != null, MAX_OBS_PER_PAGE);
         List<SimpleObject> jsonResults = new ArrayList<>(observations.results.size());
         for (Obs obs : observations.results) {
-            jsonResults.add(ObservationsHandler.obsToJson(obs));
+            jsonResults.add(obsToJson(obs));
         }
         SyncToken newToken = SyncTokenUtils.clampSyncTokenToBufferedRequestTime(
                 observations.syncToken, requestTime);
         // If we fetched a full page, there's probably more data available.
         boolean more = observations.results.size() == MAX_OBS_PER_PAGE;
         return ResponseUtil.createIncrementalSyncResults(jsonResults, newToken, more);
+    }
+
+    private SimpleObject obsToJson(Obs obs) {
+        SimpleObject object = new SimpleObject()
+            .add("uuid", obs.getUuid())
+            .add("voided", obs.isVoided());
+
+        if (obs.isVoided()) {
+            return object;
+        }
+
+        object
+            .add("patient_uuid", obs.getPerson().getUuid())
+            .add("encounter_uuid", obs.getEncounter().getUuid())
+            .add("concept_uuid", obs.getConcept().getUuid())
+            .add("timestamp", Utils.toIso8601(obs.getObsDatetime()));
+
+        Provider provider = Utils.getProviderFromUser(obs.getCreator());
+        object.add("enterer_uuid", provider != null ? provider.getUuid() : null);
+
+        boolean isExecutedOrder =
+                DbUtil.getOrderExecutedConcept().equals(obs.getConcept()) && obs.getOrder() != null;
+        if (isExecutedOrder) {
+            // As far as the client knows, a chain of orders is represented by the root order's
+            // UUID, so we have to work back through the chain or orders to get the root UUID.
+            // Normally, the client will only ever supply observations for the root order ID, but
+            // in the event that an order is marked as executed on the server (for example) we don't
+            // want that to mean that an order execution gets missed.
+            object.add("value", Utils.getRootOrder(obs.getOrder()).getUuid());
+        } else {
+            object.add("value", ObservationsHandler.obsValueToString(obs));
+        }
+
+        return object;
     }
 
     @Override
