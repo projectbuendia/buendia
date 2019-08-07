9ad833a73f91a3b28fd8d2c06497e963b4395e22
Ka-Ping Yee <zestyping@gmail.com>
2019-08-06 22:36:32 -0700

    Find patients by ID when the ID is server-generated; reject any attempt to create an MSF ID starting with "*".

--- openmrs/omod/src/main/java/org/openmrs/projectbuendia/webservices/rest/PatientResource.java
+++ openmrs/omod/src/main/java/org/openmrs/projectbuendia/webservices/rest/PatientResource.java
@@ -248,18 +248,7 @@ public class PatientResource implements Listable, Searchable, Retrievable, Creat
         synchronized (createPatientLock) {
             String id = (String) json.get(ID);
             if (id != null) {
-                List<PatientIdentifierType> identifierTypes =
-                        Collections.singletonList(DbUtil.getIdentifierTypeMsf());
-                List<Patient> existing = patientService.getPatients(
-                    null, id, identifierTypes, true /* exact identifier match */);
-                if (!existing.isEmpty()) {
-                    Patient idMatch = existing.get(0);
-                    String name = getFullName(idMatch);
-                    throw new InvalidObjectDataException(
-                        String.format(
-                            "Another patient (%s) already has the ID \"%s\"",
-                            name.isEmpty() ? "with no name" : "named " + name, id));
-                }
+                requireValidUniqueMsfIdentifier(id);
             }
             String uuid = (String) json.get(UUID);
             if (uuid != null) {
@@ -428,7 +417,8 @@ public class PatientResource implements Listable, Searchable, Retrievable, Creat
 
     private SimpleObject searchInner(String patientId) throws ResponseException {
         List<PatientIdentifierType> idTypes =
-                Collections.singletonList(DbUtil.getIdentifierTypeMsf());
+            Collections.singletonList(patientId.startsWith("*") ?
+                DbUtil.getIdentifierTypeLocal() : DbUtil.getIdentifierTypeMsf());
         List<Patient> patients = patientService.getPatients(null, patientId, idTypes, true);
         return getSimpleObjectWithResults(patients);
     }
@@ -533,18 +523,7 @@ public class PatientResource implements Listable, Searchable, Retrievable, Creat
         PatientIdentifier identifier = patient.getPatientIdentifier(DbUtil.getIdentifierTypeMsf());
         if (newId != null && !newId.isEmpty() && (identifier == null || !newId.equals(identifier.getIdentifier()))) {
             synchronized (createPatientLock) {
-                List<PatientIdentifierType> identifierTypes =
-                        Collections.singletonList(DbUtil.getIdentifierTypeMsf());
-                List<Patient> existing = patientService.getPatients(
-                    null, newId, identifierTypes, true /* exact identifier match */);
-                if (!existing.isEmpty()) {
-                    Patient idMatch = existing.get(0);
-                    String name = getFullName(idMatch);
-                    throw new InvalidObjectDataException(
-                        String.format(
-                            "Another patient (%s) already has the ID \"%s\"",
-                            name.isEmpty() ? "with no name" : "named " + name, newId));
-                }
+                requireValidUniqueMsfIdentifier(newId);
 
                 if (identifier != null) {
                     patient.removeIdentifier(identifier);
@@ -577,6 +556,27 @@ public class PatientResource implements Listable, Searchable, Retrievable, Creat
         }
         if (changedPatient) {
             patientService.savePatient(patient);
+        }
+    }
+
+    /** Verifies that a string is acceptable as a unique, well-formed identifier. */
+    private void requireValidUniqueMsfIdentifier(String ident) {
+        if (ident.startsWith("*")) {
+            throw new InvalidObjectDataException(String.format(
+                "\"%s\" is not a valid ID; the \"*\" prefix is reserved for server-generated IDs",
+                ident
+            ));
+        }
+        List<PatientIdentifierType> identifierTypes =
+            Collections.singletonList(DbUtil.getIdentifierTypeMsf());
+        List<Patient> existing = patientService.getPatients(
+            null, ident, identifierTypes, true /* exact identifier match */);
+        if (!existing.isEmpty()) {
+            String name = getFullName(existing.get(0));
+            throw new InvalidObjectDataException(String.format(
+                "Another patient (%s) already has the ID \"%s\"",
+                name.isEmpty() ? "with no name" : "named " + name, ident
+            ));
         }
     }
 }
