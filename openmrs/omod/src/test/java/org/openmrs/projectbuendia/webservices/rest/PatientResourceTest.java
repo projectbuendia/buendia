95a38c581cd08764aca05bb603e2a0dc70f37d53
Ka-Ping Yee <zestyping@gmail.com>
2019-08-13 16:37:04 -0700

    Clean up tests with BaseApiRequestTest.

--- openmrs/omod/src/test/java/org/openmrs/projectbuendia/webservices/rest/PatientResourceTest.java
+++ openmrs/omod/src/test/java/org/openmrs/projectbuendia/webservices/rest/PatientResourceTest.java
@@ -14,84 +14,45 @@
 package org.openmrs.projectbuendia.webservices.rest;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openmrs.Patient;
-import org.openmrs.api.LocationService;
-import org.openmrs.api.PatientService;
-import org.openmrs.api.context.Context;
 import org.openmrs.module.webservices.rest.SimpleObject;
-import org.openmrs.module.webservices.rest.web.response.ObjectNotFoundException;
-import org.openmrs.module.webservices.rest.web.v1_0.controller.MainResourceControllerTest;
-import org.openmrs.test.BaseModuleContextSensitiveTest;
 import org.openmrs.test.SkipBaseSetup;
 import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.fail;
 
-/** Tests for PatientResource. */
+/** REST API tests for PatientResource. */
 @SkipBaseSetup
-public class PatientResourceTest extends MainResourceControllerTest {
-
-    private static final String BASE_TEST_DATA =
-        "org/openmrs/projectbuendia/webservices/rest/base-test-data.xml";
-    private static final String PATIENT_TEST_DATA =
-        "org/openmrs/projectbuendia/webservices/rest/patient-test-data.xml";
-
+public class PatientResourceTest extends BaseApiRequestTest {
     // These constants should match the values in the BASE_TEST_DATA file.
     String PETER_PAN_UUID = "f6f74ed9-5681-482a-9aa5-c3192579fa59";
     String XANADU_UUID = "9356400c-a5a2-4532-8f2b-2361b3446eb8";
 
-    private PatientService patientService;
+    public String[] getInitialDataFiles() {
+        return new String[] {
+            "org/openmrs/projectbuendia/webservices/rest/base-test-data.xml",
+            "org/openmrs/projectbuendia/webservices/rest/patient-test-data.xml"
+        };
+    };
 
-    /**
-     * {@link BaseModuleContextSensitiveTest} does this initialization, but also pre-loads the
-     * database with a bunch of records. We don't want to load those records,
-     * because we'd then have to augment them with `buendia_[type]_sync_map` records, which would
-     * couple our test integrity to the records in OpenMRS' test data. For this reason, we disable
-     * {@link BaseModuleContextSensitiveTest}'s setup by putting the {@link SkipBaseSetup}
-     * annotation on the class, but then we've got to explicitly init the database and authenticate
-     * ourselves.
-     */
-    @Before
-    public void setUp() throws Exception {
-        patientService = Context.getPatientService();
-        if (useInMemoryDatabase()) {
-            initializeInMemoryDatabase();
-            authenticate();
-        }
-        executeDataSet(BASE_TEST_DATA);
-        executeDataSet(PATIENT_TEST_DATA);
-    }
-
-    @Override
     public String getURI() {
-        return "projectbuendia/patients";
+        return "/projectbuendia/patients";
     }
 
-    @Override
-    public String getUuid() {  // for the common tests defined in MainResourceControllerTest
+    public long getAllCount() {
+        return 0; // even though there are two patients, there are no sync timestamps
+    }
+
+    public String getUuid() {
         return PETER_PAN_UUID;
     }
 
-    @Override
-    public long getAllCount() {
-        return 2;
-    }
-
-    @Test @Override
-    public void shouldGetAll() throws Exception {
-        System.err.println("Skipping shouldGetAll(): we don't know why shouldGetAll() finds 0 patients when there should be 2.");
-    }
-
-    @Test
-    public void testGetPatient() throws Exception {
+    @Test public void testGetPatient() throws Exception {
         MockHttpServletRequest request = this.request(RequestMethod.GET, this.getURI() + "/" + PETER_PAN_UUID);
         SimpleObject response = this.deserialize(this.handle(request));
         Assert.assertNotNull(response);
@@ -103,8 +64,7 @@ public class PatientResourceTest extends MainResourceControllerTest {
         assertEquals("ABC123", response.get("id"));
     }
 
-    @Test
-    public void testCreatePatient() throws Exception {
+    @Test public void testCreatePatient() throws Exception {
         SimpleObject input = new SimpleObject();
         input.add("id", "XYZ");
         input.add("given_name", "Mary");
@@ -127,8 +87,7 @@ public class PatientResourceTest extends MainResourceControllerTest {
         assertEquals("XYZ", patient.getPatientIdentifier(2).getIdentifier());
     }
 
-    @Test
-    public void testMovePatient() throws Exception {
+    @Test public void testMovePatient() throws Exception {
         // Post an edit that sets the location of a patient.
         SimpleObject input = new SimpleObject();
         input.add("assigned_location", XANADU_UUID);
