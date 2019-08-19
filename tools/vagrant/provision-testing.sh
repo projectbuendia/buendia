242b5cd35f7a38d42e683e5c3661762d2f1f2508
Schuyler Erle <schuyler@nocat.net>
2019-08-19 13:18:26 -0700

    Set hostname when provisioning Vagrant instances, so that I can tell them apart :D

--- tools/vagrant/provision-testing.sh
+++ tools/vagrant/provision-testing.sh
@@ -3,3 +3,7 @@ apt-get install -y apt-transport-https
 echo "deb [trusted=yes] https://projectbuendia.github.io/builds/packages unstable main java" >/etc/apt/sources.list.d/buendia-github.list
 apt-get update
 apt-get install -y buendia-server buendia-site-test buendia-dashboard
+
+# Set hostname
+sed -i -e "s/$(hostname)/buendia-testing/g" /etc/hosts /etc/hostname
+hostname -F /etc/hostname
