f93b0607b989f4b0c8b71274b6a632fe7fb97f67
Schuyler Erle <schuyler@nocat.net>
2019-08-24 16:19:46 -0700

    Upgrade packages a little more aggressively when reprovisioning a Vagrant VM.

--- tools/vagrant/provision-testing.sh
+++ tools/vagrant/provision-testing.sh
@@ -2,6 +2,8 @@
 apt-get install -y apt-transport-https
 echo "deb [trusted=yes] https://projectbuendia.github.io/builds/packages unstable main java" >/etc/apt/sources.list.d/buendia-github.list
 apt-get update
+apt-get install -f -y
+apt-get upgrade -y
 apt-get install -y buendia-server buendia-site-test buendia-dashboard
 
 # Set hostname
