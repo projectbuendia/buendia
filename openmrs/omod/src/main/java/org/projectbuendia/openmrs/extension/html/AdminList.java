b3e9848639b856b8ad28c33debb01a479c135b50
Ka-Ping Yee <zestyping@gmail.com>
2020-01-07 00:44:11 -0800

    fixpom

--- openmrs/omod/src/main/java/org/projectbuendia/openmrs/extension/html/AdminList.java
+++ openmrs/omod/src/main/java/org/projectbuendia/openmrs/extension/html/AdminList.java
@@ -1,22 +1,18 @@
-//// Copyright 2015 The Project Buendia Authors
-////
-//// Licensed under the Apache License, Version 2.0 (the "License"); you may not
-//// use this file except in compliance with the License.  You may obtain a copy
-//// of the License at: http://www.apache.org/licenses/LICENSE-2.0
-////
-//// Unless required by applicable law or agreed to in writing, software distrib-
-//// uted under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
-//// OR CONDITIONS OF ANY KIND, either express or implied.  See the License for
-//// specific language governing permissions and limitations under the License.
+// Copyright 2015 The Project Buendia Authors
 //
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License.  You may obtain a copy
+// of the License at: http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distrib-
+// uted under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+// OR CONDITIONS OF ANY KIND, either express or implied.  See the License for
+// specific language governing permissions and limitations under the License.
 //package org.projectbuendia.openmrs.extension.html;
-//
 //import org.openmrs.module.Extension;
 //import org.openmrs.module.web.extension.AdministrationSectionExt;
-//
 //import java.util.LinkedHashMap;
 //import java.util.Map;
-//
 ///**
 // * This class defines the links that will appear on the administration page
 // * under the "projectbuendia.openmrs.title" heading.
