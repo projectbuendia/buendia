fcb56c1f5d19bfb8d14f3fa51253d9ac2914fd5f
Ka-Ping Yee <zestyping@gmail.com>
2020-01-07 00:36:34 -0800

    fix

--- openmrs/omod/src/main/java/org/openmrs/projectbuendia/webservices/rest/ObservationResource.java
+++ openmrs/omod/src/main/java/org/openmrs/projectbuendia/webservices/rest/ObservationResource.java
@@ -51,59 +51,6 @@ public class ObservationResource extends BaseResource<Obs> {
         return obsService.getObsByUuid(uuid);
     }
 
-    private SimpleObject abbreviateResult(SimpleObject result) {
-        final int MAX_ITEMS = 10;
-        List<SimpleObject> items = (List<SimpleObject>) result.get("results");
-        List<Object> abbrevItems = new ArrayList<>();
-        Object resultsValue = items;
-
-        if (items.size() > MAX_ITEMS) {
-            int omitted = items.size() - MAX_ITEMS;
-            abbrevItems.add(String.format(
-                "...only logging last %d of %d items (%d omitted)...",
-                items.size(), MAX_ITEMS, omitted
-            ));
-            abbrevItems.addAll(items.subList(omitted, items.size()));
-            resultsValue = abbrevItems;
-        }
-
-        SimpleObject abbrev = new SimpleObject()
-            .add("results", resultsValue)
-            .add("syncToken", result.get("syncToken"))
-            .add("more", result.get("more"));
-    }
-
-    private SimpleObject obsToJson(Obs obs) {
-        SimpleObject object = new SimpleObject()
-            .add("uuid", obs.getUuid())
-            .add("voided", obs.isVoided());
-
-        if (obs.isVoided()) {
-            return object;
-        }
-
-        object
-            .add("patient_uuid", obs.getPerson().getUuid())
-            .add("encounter_uuid", obs.getEncounter().getUuid())
-            .add("concept_uuid", obs.getConcept().getUuid())
-            .add("timestamp", Utils.toIso8601(obs.getObsDatetime()));
-
-        Provider provider = Utils.getProviderFromUser(obs.getCreator());
-        object.add("enterer_uuid", provider != null ? provider.getUuid() : null);
-
-        boolean isExecutedOrder =
-                DbUtil.getOrderExecutedConcept().equals(obs.getConcept()) && obs.getOrder() != null;
-        if (isExecutedOrder) {
-            // As far as the client knows, a chain of orders is represented by the root order's
-            // UUID, so we have to work back through the chain or orders to get the root UUID.
-            // Normally, the client will only ever supply observations for the root order ID, but
-            // in the event that an order is marked as executed on the server (for example) we don't
-            // want that to mean that an order execution gets missed.
-            object.add("value", Utils.getRootOrder(obs.getOrder()).getUuid());
-        } else {
-            object.add("value", ObservationUtils.obsValueToString(obs));
-        }
-
     @Override protected void deleteItem(Obs obs, String reason, RequestContext context) {
         obsService.voidObs(obs, reason + " (from Buendia client)");
     }
